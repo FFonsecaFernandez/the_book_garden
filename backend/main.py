2e79b3fa5ff530599f2e2b734ed65323eb3591db
Francisco Fonseca <fonsecafer12@gmail.com>
2024-03-16 02:37:19 -0300

    avance del proyecto

--- backend/main.py
+++ backend/main.py
@@ -8,44 +8,60 @@ from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import sqlite3
 
-#app = FastAPI()
+app = FastAPI()
 
-#app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
-#template = Jinja2Templates(directory="../frontend")
+app.mount("/static", StaticFiles(directory="../frontend/static"), name="static")
+app.mount("/images", StaticFiles(directory="../frontend/static/images"), name="images")
+template = Jinja2Templates(directory="../frontend")
 
-#@app.get("/")
-#async def read_root(req: Request):
-#    return template.TemplateResponse("index.html", {"request":req,
-#                                     "message":"prueba html con fastapi y jinja"})
+@app.get("/")
+async def read_root(req: Request):
+    return template.TemplateResponse("index.html", {"request":req})
 
-#@app.get("/login")
-#async def root_login(req: Request):
-#    return template.TemplateResponse("login.html", {"request":req,
-#                                     "message":"prueba html con fastapi y jinja"})
+@app.get("/login")
+async def root_login(req: Request):
+    return template.TemplateResponse("login.html", {"request":req})
 
+@app.post("/login/usercrate")
+async def create_user(nombre,apellido,edad,email):
+    conexion = sqlite3.connect("user.db")
+    try:
+        conexion.execute("""create table user (
+                              codigo integer primary key autoincrement,
+                              Nombre text,
+                              Apellido text,
+                              Edad integer,
+                              Email text
+                        )""")
+        print("se creo la tabla user")                        
+    except sqlite3.OperationalError:
+        print("La tabla user ya existe")  
+    
+    conexion.execute("insert into user(Nombre,Apellido,Edad,Email) values (?,?,?,?)", (nombre,apellido,edad,email))
+    conexion.commit()
+    cursor=conexion.execute("select codigo,Nombre,Apellido,Edad,Email from user")
+    for fila in cursor:
+        print(fila, "esta es la base de datos")
+    conexion.close()
+    
 
-#class Item(BaseModel):
-#    name: str
-#    price: float
-#    is_offer: Union[bool, None] = None
-
-#@app.get("/items/{item_id}")
-#async def read_item(item_id: int, q: Union[str, None] = None):
-#    return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
 #@app.put("/items/{item_id}")
-#def update_item(item_id: int, item: Item):
+#def update_item(item_id: int, item:"Item"):
 #    return {"item_name": item.name, "item_id": item_id}
 
 # db sqlite3
 
 
-conexion=sqlite3.connect("cover.db")
+#conexion=sqlite3.connect("cover.db")
 
 
-conexion.execute("insert into cover_book(title,cover_url) values (?,?)", ("feo", "feo.jpg"))
-conexion.commit()
-cursor=conexion.execute("select codigo,title,cover_url from cover_book")
-for fila in cursor:
-    print(fila, "esta es la base de datos")
-conexion.close()
+#conexion.execute("insert into cover_book(title,cover_url) values (?,?)", ("feo", "feo.jpg"))
+#conexion.commit()
+#cursor=conexion.execute("select codigo,title,cover_url from cover_book")
+#for fila in cursor:
+#    print(fila, "esta es la base de datos")
+#conexion.close()
--- frontend/static/login.js
+++ frontend/static/login.js
@@ -8,6 +8,15 @@ function regresar(){
 
 function inicioSesion() {
     document.getElementById("login").style.display = "none";
+    document.getElementById("singin").style.backgroundColor = "rgb(250,250,250)"; 
     document.getElementById("singin").style.display = "flex";
 }
     
+/*function save_user() {
+    var nombre = document.getElementsByTagName("Nombre").value
+    document.getElementById("log_container").style.backgroundColor = "rgb(0,0,0)";
+    var h2 = document.querySelector("h2");
+    var text = document.createTextNode(nombre);
+    h2.append(text);
+}
+*/
